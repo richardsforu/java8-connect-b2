e30abbe40a1bd33896e862ada167fb0d475b35cb
richardsforu <praveen.somireddy@gmail.com>
2021-04-01 11:21:15 +0530

    Java 8 day 2

--- java8-ws/DataTimeAPI/src/com/C1.java
+++ java8-ws/DataTimeAPI/src/com/C1.java
@@ -1,11 +1,29 @@
 package com;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 public class C1 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		
+		String date="2021-01-25";
+		LocalDate dd=LocalDate.parse(date);
+		
+		String d2=dd.format(DateTimeFormatter.ofPattern("dd-MMMM-yy"));
+		System.out.println(d2);
+		
+		System.out.println(dd);
+		
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-mm-dd");
+	
+		
+		
 		
 		LocalDate ld=LocalDate.now();
 		System.out.println(ld);
